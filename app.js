b11e0b5b2e1f157d63ecd94c6556f4ce1e9b2dc2
Hannah Riley <riley.hannahm@gmail.com>
2020-02-19 11:55:07 -0800

    its lunch time

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const data = require('./geo.js');
+const app = express();
+const request = require('superagent');
+const cors = require('cors');
+
+app.get('/', (request, respond) => respond.send('Hello World!'));
+const port = process.env.PORT || 3000;
+
+app.get('/location/', (request, respond) => {
+    const cityData = data.results[0];
+    respond.json({
+        formatted_query: cityData.formatted_address,
+        latitude: cityData.geometry.location.lat,
+        longitude: cityData.geometry.location. lng
+        
+    });
+})
+
+app.get('/weather', (request, respond) => {
+    // use lat and long from earlier to get weather data for the selected area
+    
+    // res.json that weather data in the appropriate form
+})
+
+app.get('*', (request, respond) => respond.send('404!!'));
+
+// when testing this needs to move to index.js
+app.listen(port, () => {
+    console.log('running . . . ', port)});
+
+    module.exports = { app };
